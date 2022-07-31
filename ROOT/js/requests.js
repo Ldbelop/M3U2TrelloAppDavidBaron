e0897eda3f679bf4e8e87d6563b6b6d196fc747a
David Barón <baronavilandavid@gmail.com>
2022-07-31 13:37:05 -0500

    Primeros pasos de creacion de API con axios get

--- ROOT/js/requests.js
+++ ROOT/js/requests.js
@@ -1,2 +1,6 @@
-axios('')
-    .then((data) => {console.log(data)})
+function showAllCards(cards){
+    cards.map((card) => console.log(card))
+}
+
+axios.get('https://my-json-server.typicode.com/Ldbelop/M3U2TrelloAppDavidBaron/db')
+    .then((result) => {showAllCards(result.data.examples)})
